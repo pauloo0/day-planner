602a8c118f1aaf62c2ffe2a3f8272845b5effbb7
Paulo Oliveira <oliveirapaulo00@hotmail.com>
2024-01-13 10:57:58 +0000

    Installing mantine

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import Calendar from './features/Calendar/Calendar'
 import MeetingList from './features/Meeting/MeetingList'
 import MeetingCreateForm from './features/Meeting/MeetingCreateForm'
 
+import '@mantine/core/styles.css'
+import { MantineProvider } from '@mantine/core'
+
 const App: React.FC = () => {
   const [isFormOpen, setIsFormOpen] = useState(false)
 
   return (
-    <div>
+    <MantineProvider>
       <Calendar />
       <MeetingList />
       <button type='button' onClick={() => setIsFormOpen(!isFormOpen)}>
         {isFormOpen ? 'Cancelar' : 'Novo'}
       </button>
       {isFormOpen && <MeetingCreateForm setIsFormOpen={setIsFormOpen} />}
-    </div>
+    </MantineProvider>
   )
 }
 
